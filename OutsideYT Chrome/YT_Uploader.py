a2e3b614d0226337550216662218b0e15155e6c6
Outsider <romikfe@mail.ru>
2023-03-18 00:47:07 +0300

    update upload func, rework settings.py

--- OutsideYT Chrome/YT_Uploader.py
+++ OutsideYT Chrome/YT_Uploader.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 import subprocess
 import time
 
@@ -11,6 +12,10 @@ import webbrowser
 import pickle
 
 from oyt_info.settings import Settings
+
+text_extensions = [".txt"]
+video_extensions = [".mp4", ".avi"]
+image_extensions = [".pjp", ".jpg", ".pjpeg", ".jpeg", ".jfif", ".png"]
 
 
 def google_login(login: str, driver: webdriver):
@@ -39,8 +44,28 @@ def google_login(login: str, driver: webdriver):
         driver.quit()
 
 
-def upload_video(driver, user, video, preview=None, title=None, description=None, playlist=None, tags=None, ends=None,
-                 access=0, publ_time=0, save_title=False, def_title=False):
+def upload_video(driver, user, video, preview=None, title=None,
+                 description=None, playlist=None, tags=None,
+                 ends="random", end_duration=20,
+                 access=0, publ_time=0, save_title=False):
+    """
+    :param driver: Объект драйвера
+    :param user: Имя пользователя
+    :param video: папка с данными для видео
+    :param preview: Превью
+    :param title: Название
+    :param description: Описание
+    :param playlist: Плейлист
+    :param tags: теги
+    :param ends: import (default) - импортировать конечные заставки из другого видео (рандомного),
+    random - рандомные конечные заставки из стандартных
+    :param end_duration: длительность конечных заставок (20)
+    :param access: 0 - приватное, 1 - доступ по ссылке, 2 - открытое
+    :param publ_time:
+    :param save_title: Bool - использовать ли название файла с видео в качестве названия
+    :param save_preview: Bool - использовать ли стандартное превью
+    :return:
+    """
     url = "https://youtube.com"
     url2 = "https://studio.youtube.com/"
     driver.get(url)
@@ -50,31 +75,36 @@ def upload_video(driver, user, video, preview=None, title=None, description=None
     driver.implicitly_wait(7)
 
     driver.get(url2)
-    driver.implicitly_wait(7)
+    driver.implicitly_wait(4)
 
     driver.find_element(By.XPATH, '//*[@id="upload-icon"]').click()
     driver.implicitly_wait(7)
 
-    path = f"videos/{user}/{video}/"
-    vid = find_files(".mp4", ".avi", folder=path)
-    driver.find_element(By.XPATH, '//*[@id="content"]/input').send_keys(os.path.abspath(path + vid))
+    path = os.path.join(settings.vids_folder, user, video)
+    print(path)
+    vid = find_files(video_extensions, folder=path)
+    driver.find_element(By.XPATH, '//*[@id="content"]/input').send_keys(os.path.abspath(os.path.join(path, vid)))
     driver.implicitly_wait(7)
 
-    if not save_title:
-        if not title:
-            title = find_files(".txt", folder=path, name="Title")
-        title_el = driver.find_element(By.XPATH, f'//*[@id="title-textarea"]/'
-                                                 f'*[@id="container"]/*[@id="outer"]/*[@id="child-input"]/'
-                                                 f'*[@slot="body"]/*[@id="input"]/div')
-        if title_el.text == ".".join(vid.split(".")[:-1]) or not def_title:
-            title_el.clear()
-            title_el.send_keys(title)
+    if not title:
+        title = find_files(text_extensions, folder=path, name="Title")
+    title_el = driver.find_element(By.XPATH, f'//*[@id="title-textarea"]/'
+                                             f'*[@id="container"]/*[@id="outer"]/*[@id="child-input"]/'
+                                             f'*[@slot="body"]/*[@id="input"]/div')
+    if title:
+        time.sleep(1)
+        title_el.clear()
+        title_el.send_keys(title)
+    elif title_el.text != ".".join(vid.split(".")[:-1]) and save_title:
+        title_el.clear()
+        title_el.send_keys(".".join(vid.split(".")[:-1]))
 
     if not description:
         description = find_files(".txt", folder=path, name="Description")
     description_el = driver.find_element(By.XPATH, f'//*[@id="description-textarea"]/'
                                                    f'*[@id="container"]/*[@id="outer"]/*[@id="child-input"]/'
                                                    f'*[@slot="body"]/*[@id="input"]/div')
+    time.sleep(5)
     description_old = description_el.text
     description_el.clear()
     if description_old:
@@ -83,17 +113,99 @@ def upload_video(driver, user, video, preview=None, title=None, description=None
         descs_del = ""
     description_el.send_keys("".join([description, descs_del, description_old]))
 
+    if not preview:
+        preview = find_files(image_extensions, folder=path)
+    if preview:
+        try:
+            driver.find_element(By.XPATH, f'//input[@id="file-loader"]').send_keys(
+                os.path.abspath(os.path.join(path, preview)))
+            driver.implicitly_wait(7)
+        except Exception:
+            print("Превью невозможно загрузить")
+
+    # Добавить добавление в плейлисты
+
+    if not tags:
+        tags = find_files(text_extensions, folder=path, name="Tags")
+    if tags:
+        driver.find_element(By.XPATH, f'//*[@id="toggle-button"]').click()
+        driver.implicitly_wait(4)
+        tags_el = driver.find_element(By.XPATH, f'//*[@id="tags-container"]/'
+                                                f'*[@id="outer"]/*[@id="child-input"]/'
+                                                f'*[@slot="body"]/*[@id="chip-bar"]/div/*[@id="text-input"]')
+        tags_el.send_keys(tags)
+        driver.implicitly_wait(4)
+
+    driver.find_element(By.XPATH, f'//*[@id="next-button"]').click()
+    driver.implicitly_wait(4)
+    if ends:
+        try:
+            driver.find_element(By.XPATH, f'//*[@id="endscreens-button"]').click()
+            driver.implicitly_wait(4)
+
+            if ends == "import":
+                pass
+            elif ends == "random":
+                ends_el = driver.find_elements(By.XPATH,
+                                               f'//*[@class="card style-scope ytve-endscreen-template-picker"]')
+                print(ends_el)
+                while True:
+                    end_num = random.randint(0, len(ends_el) - 1)
+                    print(end_num)
+                    if "Playlist" not in \
+                            ends_el[end_num].find_element(By.XPATH,
+                            f'//*[@class="title style-scope ytve-endscreen-template-picker]').text\
+                            and "Плейлист" not in \
+                            ends_el[end_num].find_element(By.XPATH,
+                            f'//*[@class="title style-scope ytve-endscreen-template-picker]').text:
+                        ends_el[end_num].click()
+                        break
+
+            driver.implicitly_wait(4)
+            time.sleep(2)
+            driver.find_element(By.XPATH, f'//*[@id="save-button"]').click()
+            driver.implicitly_wait(4)
+        except Exception as e:
+            print(f"Невозможно поставить конечные заставки\n"
+                  f"{e}")
+
+    driver.find_element(By.XPATH, f'//*[@id="next-button"]').click()
+    driver.implicitly_wait(4)
+
+
     time.sleep(40)
 
 
-def find_files(*args, folder, name: str = ""):
+def find_files(args: list, folder: str, name: str = ""):
     for file in os.listdir(folder):
-        if file.endswith(args) and file.startswith(name):
-            if args.count(".txt"):
-                with open(folder + file, "r", encoding="UTF-8") as f:
+        if file.endswith(tuple(args)) and file.startswith(name):
+            if ".txt" in args:
+                with open(os.path.join(folder, file), "r", encoding="UTF-8") as f:
                     return f.read()
             return file
     print("File not founded")
+    return None
+
+
+def change_def_title(account: str, title: str):
+    pass
+
+
+def change_def_description(account: str, description: str):
+    pass
+
+
+def change_def_tags(account: str, tags):
+    pass
+
+
+def change_def_access(account: str, access: int):
+    """
+    :param account: Название аккаунта
+    :param access: 0 - приватное, 1 - доступ по ссылке, 2 - открытое
+    :return:
+    """
+    pass
 
 
 def start():
--- OutsideYT Chrome/oyt_info/settings.py
+++ OutsideYT Chrome/oyt_info/settings.py
@@ -2,18 +2,114 @@ import os
 
 
 class Settings:
-
     def __init__(self):
         self._accounts = []
         self._def_account = ""
-        self._def_access = 0
+        self._vids_folder = "videos"
+
+    @property
+    def accounts(self):
+        return self._accounts
+
+    @property
+    def vids_folder(self):
+        return self._vids_folder
+
+    @property
+    def def_account(self):
+        return self._def_account
+
+    def add_account(self, login):
+        if self.accounts.count(login) == 0:
+            self._accounts.append(login)
+            self.update_settings()
+        else:
+            print("Аккаунт с таким именем уже существует")
+        return
+
+    def del_account(self, login):
+        self._accounts.remove(login)
+        return
+
+    def add_vids_folder(self, path):
+        self._vids_folder = path
+        self.update_settings()
+        return
+
+    def del_vids_folder(self):
+        self._vids_folder = "/videos"
+        self.update_settings()
+        return
+
+    def add_def_account(self, val):
+        self._def_account = val
+        self.update_settings()
+        return
+
+    def del_def_account(self):
+        self._def_account = ""
+        self.update_settings()
+        return
+
+    def create_videos_dir(self):
+        path = self.vids_folder
+        if not os.path.exists(path):
+            os.mkdir(path)
+        for login in self.accounts:
+            if not os.path.exists(f'{path}/{login}'):
+                os.mkdir(f'{path}/{login}')
+        return
+
+    def update_settings(self):
+        with open("oyt_info/settings", "w") as file:
+            file.writelines([f'accounts={",".join(self.accounts)}\n',
+                             f'vids_folder={self.vids_folder}\n',
+                             f'def_account={self.def_account}\n'])
+        self.create_videos_dir()
+
+    def read_settings(self, file):
+        if not os.path.exists(file):
+            print("Такого файла не существует")
+            return
+        with open(file, "r") as file:
+            line = file.readline().strip()
+            while line != "":
+                if line.find("accounts=") != -1:
+                    self._accounts = line.split("=")[1].split(",")
+                elif line.find("def_account") != -1:
+                    self._def_account = line.split("=")[1]
+                elif line.find("vids_folder") != -1:
+                    self._vids_folder = line.split("=")[1]
+                line = file.readline().strip()
+        return
+
+    @staticmethod
+    def check_cookies():
+        accs = []
+        for file in os.listdir():
+            if file.endswith("_cookies"):
+                accs.append(file.replace("_cookies", ""))
+        out = ""
+        with open("oyt_info/settings", "r") as f:
+            for line in f:
+                if line.find("accounts=") != -1:
+                    out += "accounts=" + ",".join(accs) + "\n"
+                else:
+                    out += line
+        print(out, file=open("oyt_info/settings", "w", encoding="UTF-8"))
+
+
+class AccountSettings:
+
+    def __init__(self, account):
+        self._account = account
+        # self._def_access = 0
         self._def_playlist = ""
-        self._vids_folder = "videos/"
-        self._def_title = ""
-        self._def_description = ""
-        self._def_tags = []
-        self._def_ends = []
-        self._def_preview = ""
+        # self._def_title = ""
+        # self._def_description = ""
+        # self._def_tags = []
+        self._def_ends = 20
+        # self._def_preview = ""
         self._def_publ_time = 0
         if os.path.exists("oyt_info/settings"):
             self.read_settings("oyt_info/settings")
@@ -28,7 +124,7 @@ class Settings:
                              f'def_description={self._def_description}\n',
                              f'def_playlist={self._def_playlist}\n',
                              f'def_tags={",".join(self._def_tags)}\n',
-                             f'def_ends={",".join(self._def_ends)}\n',
+                             f'def_ends={self._def_ends}\n',
                              f'def_preview={self._def_preview}\n',
                              f'def_access={self._def_access}\n',
                              f'def_publ_time={self._def_publ_time}\n'])
@@ -56,7 +152,7 @@ class Settings:
                 elif line.find("def_tags") != -1:
                     self._def_tags = line.split("=")[1].split(",")
                 elif line.find("def_ends") != -1:
-                    self._def_ends = line.split("=")[1].split(",")
+                    self._def_ends = line.split("=")
                 elif line.find("def_preview") != -1:
                     self._def_preview = line.split("=")[1]
                 elif line.find("def_access") != -1:
@@ -142,20 +238,50 @@ class Settings:
                 os.mkdir(f'{path}/{login}')
         return
 
-    @staticmethod
-    def check_cookies():
-        accs = []
-        for file in os.listdir():
-            if file.endswith("_cookies"):
-                accs.append(file.replace("_cookies", ""))
-        out = ""
-        with open("oyt_info/settings", "r") as f:
-            for line in f:
-                if line.find("accounts=") != -1:
-                    out += "accounts=" + ",".join(accs) + "\n"
-                else:
-                    out += line
-        print(out, file=open("oyt_info/settings", "w", encoding="UTF-8"))
+    @property
+    def accounts(self):
+        return self._accounts
+
+    @property
+    def vids_folder(self):
+        return self._vids_folder
+
+    @property
+    def def_account(self):
+        return self._def_account
+
+    @property
+    def def_title(self):
+        return self._def_title
+
+    @property
+    def def_description(self):
+        return self._def_description
+
+    @property
+    def def_playlist(self):
+        return self._def_playlist
+
+    @property
+    def def_preview(self):
+        return self._def_preview
+
+    @property
+    def def_tags(self):
+        return self._def_tags
+
+    @property
+    def def_access(self):
+        return self._def_access
+
+    @property
+    def def_ends(self):
+        return self._def_ends
+
+    @property
+    def def_publ_time(self):
+        return self._def_publ_time
+
 
 if os.path.exists("oyt_info/settings"):
     Settings.check_cookies()
